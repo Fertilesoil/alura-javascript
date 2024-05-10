37ad96600338cf2bab6641faef56ba5588a60eb5
Fertilesoil <gasnbloop@gmail.com>
2024-05-10 14:26:02 -0300

    Início do peojeto focus

---
+++ fokus-curso/main.js
@@ -0,0 +1,22 @@
+﻿const html = document.querySelector("html");
+const focoBtn = document.querySelector(".app__card-button--foco");
+const curtoBtn = document.querySelector(".app__card-button--curto");
+const longoBtn = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
+
+focoBtn.onclick = () => {
+  alterarContexto("foco");
+}
+
+curtoBtn.onclick = () => {
+  alterarContexto("descanso-curto");
+}
+
+longoBtn.onclick = () => {
+  alterarContexto("descanso-longo");
+}
+
+const alterarContexto = (contexto) => {
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `./imagens/${contexto}.png`);
+}
