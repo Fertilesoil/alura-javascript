fc09ed7f566e98cdc517f4baab7000fa58f17c6a
Fertilesoil <gasnbloop@gmail.com>
2024-05-10 13:09:29 -0300

    Finalização do projeto aluramidi

---
+++ aluramidi-curso/main.js
@@ -0,0 +1,22 @@
+﻿const botoes = document.querySelectorAll(".tecla");
+
+const tocarInstrumento = (instrumento) => {
+  return document.querySelector(`#som_tecla_${instrumento}`).play();
+}
+
+botoes.forEach((botao) => {
+  const instrumento = botao.classList[1].split("_")[1];
+  botao.onclick = () => tocarInstrumento(instrumento);
+  
+  botao.onkeydown = (e) => {
+    if (e.code === "Enter" || e.code === "Space") {
+    botao.classList.toggle("ativa");
+    }
+  }
+
+  botao.onkeyup = (e) => {
+    if (e.code === "Enter" || e.code === "Space") {
+    botao.classList.toggle("ativa");
+    }
+  }
+});
